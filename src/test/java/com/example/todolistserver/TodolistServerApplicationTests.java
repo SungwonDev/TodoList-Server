e8ba4e754a28f617bad19d0ab6d603814779d8b9
Sungwon Lee <sstxz444@gmail.com>
2023-02-27 18:42:53 +0900

    TodoServiceTest > add
    TodoServiceTest> add완성

--- src/test/java/com/example/todolistserver/TodolistServerApplicationTests.java
+++ src/test/java/com/example/todolistserver/TodolistServerApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example.todolistserver;
 
+import com.example.todolistserver.model.TodoEntity;
+import com.example.todolistserver.model.TodoRequest;
+import com.example.todolistserver.repository.TodoRepository;
+import com.example.todolistserver.service.TodoService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
 class TodolistServerApplicationTests {
 
+	@InjectMocks
+	private TodoService todoService;
+
+	@Mock
+	private TodoRepository todoRepository;
+
 	@Test
-	void contextLoads() {
+	public void add() {
+		when(this.todoRepository.save(any(TodoEntity.class)))
+				.then(AdditionalAnswers.returnsFirstArg());
+
+		TodoRequest request = new TodoRequest();
+		request.setTitle("test title");
+		TodoEntity actual = this.todoService.add(request);
+
+		//assertEquals(1L, actual.getId());
+		assertEquals("test title", actual.getTitle());
+
+
 	}
 
 }
